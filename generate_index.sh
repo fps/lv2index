ee9ccf7b1c56260372acf8877a72ab3b30db095f
Florian Paul Schmidt <mista.tapas@gmx.net>
2022-09-21 08:45:19 +0200

    use a table

--- generate_index.sh
+++ generate_index.sh
@@ -1,9 +1,10 @@
-echo '<html><head></head><body>' > index.html; 
+echo '<html><head></head><body><table>' > index.html; 
 for n in $(lv2ls); do 
+  echo '<tr>' >> index.html
   filename=$(echo info/$(echo $(echo $n | sha256sum) | cut -d ' ' -f 1).html); 
   name=$(cat $filename | head -n4 | tail -n 1 | tr -s ' ' | cut -d ' ' -f 2-)
   uri=$(head -n2 $filename | tail -n1)
   echo "index entry for $filename $name $uri"
-  echo "<p><a href=\"$filename\">$name ($uri)</a></p>" >> index.html;  
+  echo "<td><a href=\"$filename\">$name</a></td><td>$uri</td>" >> index.html;  
 done; 
 echo '</body></html>' >> index.html
