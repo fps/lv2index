70d81d1e274c83711e74b7acd704e7ade8dc45a7
Florian Paul Schmidt <mista.tapas@gmx.net>
2022-09-21 08:42:43 +0200

    fix index generation

--- generate_index.sh
+++ generate_index.sh
@@ -1,1 +1,9 @@
-echo '<html><head></head><body>' > index.html; for n in $(lv2ls); do filename=$(echo info/$(echo $(echo $n | sha256sum) | cut -d ' ' -f 1).html); echo "<p><a href=\"$filename\">$(cat $filename | head -n3 | tail -n 1 | tr -s ' ' | cut -d ' ' -f 2-) $(head -n1 $filename)</a></p>" >> index.html;  done; echo '</body></html>' >> index.html
+echo '<html><head></head><body>' > index.html; 
+for n in $(lv2ls); do 
+  filename=$(echo info/$(echo $(echo $n | sha256sum) | cut -d ' ' -f 1).html); 
+  name=$(cat $filename | head -n4 | tail -n 1 | tr -s ' ' | cut -d ' ' -f 2-)
+  uri=$(head -n2 $filename | tail -n1)
+  echo "index entry for $filename $name $uri"
+  echo "<p><a href=\"$filename\">$name ($uri)</a></p>" >> index.html;  
+done; 
+echo '</body></html>' >> index.html
